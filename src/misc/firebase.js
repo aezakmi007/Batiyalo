23d8e920e0bb4c6d2c3e198ec9f56b53b5d77f31
aezakmi007 <abdullahfarhan910@gmail.com>
2021-05-17 10:17:19 +0530

    initialised firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyBf4WdbsKOUvn_wrQs9eZ1HlfLx9qyH1Kw',
+  authDomain: 'batiyalo-e6931.firebaseapp.com',
+  projectId: 'batiyalo-e6931',
+  storageBucket: 'batiyalo-e6931.appspot.com',
+  messagingSenderId: '502599654045',
+  appId: '1:502599654045:web:371daafb400a01ecd09274',
+};
+const app = firebase.initializeApp(config);
