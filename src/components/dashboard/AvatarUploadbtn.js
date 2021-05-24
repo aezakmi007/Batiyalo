7d8d9182aa9d007808106b22cf04c2e1128ce9ab
aezakmi007 <abdullahfarhan910@gmail.com>
2021-05-24 06:57:26 +0530

    update all user refrences if data is updated

--- src/components/dashboard/AvatarUploadbtn.js
+++ src/components/dashboard/AvatarUploadbtn.js
@@ -5,6 +5,7 @@ import useModalState from '../../misc/custom-hooks';
 import { database, storage } from '../../misc/firebase';
 import useProfile from '../../context/profile.context';
 import ProfileAvatar from '../ProfileAvatar';
+import { getUserUpdates } from '../../misc/Helper';
 
 const fileInputTypes = '.png, .jpeg, .jpg';
 
@@ -62,11 +63,20 @@ function AvatarUploadbtn() {
       });
 
       const downloadUrl = await uploadAvatarResult.ref.getDownloadURL();
-      const userAvatarRef = database
-        .ref(`/profiles/${profile.uid}`)
-        .child('avatar');
 
-      userAvatarRef.set(downloadUrl);
+      const updates = await getUserUpdates(
+        profile.uid,
+        'avatar',
+        downloadUrl,
+        database
+      );
+      await database.ref().update(updates);
+
+      // const userAvatarRef = database
+      //   .ref(`/profiles/${profile.uid}`)
+      //   .child('avatar');
+
+      // userAvatarRef.set(downloadUrl);
 
       setIsLoading(false);
       Alert.info('Avatar has been uploaded', 4000);
