0dd0a4ef8d81cce0bc82a42c5494c4ca62aed643
aezakmi007 <abdullahfarhan910@gmail.com>
2021-05-24 07:15:54 +0530

    displayed messages inside chat

--- src/components/chat-window/messages/index.js
+++ src/components/chat-window/messages/index.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import { database } from '../../../misc/firebase';
+import { transformToArrayWithId } from '../../../misc/Helper';
+import MessageItem from './MessageItem';
 
 function Messages() {
-  return <div>Messages</div>;
+  const { chatId } = useParams();
+  const [messages, setMessages] = useState(null);
+
+  const isChatEmpty = messages && messages.length === 0;
+  const canShowMessages = messages && messages.length > 0;
+
+  useEffect(() => {
+    const messageRef = database.ref('/messages');
+
+    messageRef
+      .orderByChild('roomId')
+      .equalTo(chatId)
+      .on('value', snap => {
+        const data = transformToArrayWithId(snap.val());
+
+        setMessages(data);
+      });
+
+    return () => {
+      messageRef.off('value');
+    };
+  }, [chatId]);
+  return (
+    <ul className="msg-list custom-scroll">
+      {isChatEmpty && <li>No Messages ye</li>}
+      {canShowMessages &&
+        messages.map(msg => <MessageItem key={msg.id} message={msg} />)}
+    </ul>
+  );
 }
 
 export default Messages;
