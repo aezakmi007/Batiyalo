28d967c5c155ab8d98cae03b4d7a5dc77aca7312
aezakmi007 <abdullahfarhan910@gmail.com>
2021-05-27 08:28:16 +0530

    Added room edit drawer

--- src/components/EditableInput.js
+++ src/components/EditableInput.js
@@ -7,6 +7,7 @@ function EditableInput({
   label = null,
   placeholder = 'write your value',
   emptyMessage = 'Input is Empty',
+  wrapperClassName = '',
   ...inputProps
 }) {
   const [input, setInput] = useState(initialValue);
@@ -33,7 +34,7 @@ function EditableInput({
     setIsEditable(false);
   };
   return (
-    <div>
+    <div className={wrapperClassName}>
       {label}
       <InputGroup>
         <Input
--- src/components/chat-window/top/index.js
+++ src/components/chat-window/top/index.js
@@ -4,6 +4,7 @@ import { ButtonToolbar, Icon } from 'rsuite';
 
 import { useCurrentRoom } from '../../../context/current-room.context';
 import { useMediaQuery } from '../../../misc/custom-hooks';
+import EditRoomBtnDrawer from './EditRoomBtnDrawer';
 import RoomIfoBtmModal from './RoomIfoBtmModal';
 
 function ChatTop() {
@@ -28,6 +29,7 @@ function ChatTop() {
           <span className="text-disappear">{name}</span>
         </h4>
         <ButtonToolbar className="ws-nowrap">todo</ButtonToolbar>
+        <EditRoomBtnDrawer />
       </div>
 
       <div className="d-flex justify-content-between align-items-center">
