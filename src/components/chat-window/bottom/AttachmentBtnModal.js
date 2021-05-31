fd0d1f30dbbfaf62413149edef39848b47f85931
aezakmi007 <abdullahfarhan910@gmail.com>
2021-05-31 12:08:16 +0530

    Error Promie.add is not a function

---
+++ src/components/chat-window/bottom/AttachmentBtnModal.js
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import { useParams } from 'react-router';
+import { Alert, Button, Icon, InputGroup, Modal, Uploader } from 'rsuite';
+import useModalState from '../../../misc/custom-hooks';
+import { storage } from '../../../misc/firebase';
+
+const MAX_FILE_SIZE = 1000 * 1024 * 5;
+
+function AttachmentBtnModal({ afterUpload }) {
+  const { chatId } = useParams();
+  const { isOpen, close, open } = useModalState();
+
+  const [fileList, setFileList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const onChange = fileArr => {
+    const filtered = fileArr
+      .filter(el => el.blobFile.size <= MAX_FILE_SIZE)
+      .slice(0, 5);
+
+    setFileList(filtered);
+  };
+
+  const onUpload = async () => {
+    try {
+      const uploadPromises = fileList.map(f => {
+        return storage
+          .ref(`/chat/${chatId}`)
+          .child(Date.now() + f.name)
+          .put(f.blobFile, {
+            cacheControl: `public, max-age=${3600 * 24 * 3}`,
+          });
+      });
+
+      const uploadSnapShot = await Promise.all(uploadPromises);
+
+      const shapePromises = uploadSnapShot.map(async snap => {
+        return {
+          contentType: (await snap).metadata.contentType,
+          name: snap.metadata.name,
+          url: await snap.ref.getDownloadURL(),
+        };
+      });
+
+      const files = await Promise.add(shapePromises);
+      await afterUpload(files);
+      setIsLoading(false);
+      close();
+    } catch (err) {
+      setIsLoading(false);
+      Alert.error(err.message);
+    }
+  };
+
+  return (
+    <>
+      <InputGroup.Button onClick={open}>
+        <Icon icon="attachement" />
+      </InputGroup.Button>
+      <Modal show={isOpen} onHide={close}>
+        <Modal.Header>
+          {' '}
+          <Modal.Title>Upload Files</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Uploader
+            autoUpload={false}
+            action=""
+            fileList={fileList}
+            onChange={onChange}
+            multiple
+            listType="picture-text"
+            className="w-100"
+            disabled={isLoading}
+          />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button disabled={isLoading} onClick={onUpload} block>
+            Send to Chat
+          </Button>
+
+          <div className="text-right mt-2">
+            <small>* only files less than 5mb are allowed</small>
+          </div>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default AttachmentBtnModal;
